13df24c6147a5575fc4f9ca54a49e004c830f254
MOZGIII <mike-n@narod.ru>
2023-03-16 21:31:18 +0400

    Hash searching support

--- src/hooks/useSearchSuggestions.ts
+++ src/hooks/useSearchSuggestions.ts
@@ -1,3 +1,4 @@
+import { useApolloClient } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { suggest, Suggestion } from "../lib/search";
 
@@ -6,10 +7,12 @@ export type SearchSuggestionsData = null | Suggestion[];
 
 const useSearchSuggestions = (input: string): SearchSuggestionsData => {
   const [suggestions, setSuggestions] = useState<SearchSuggestionsData>(null);
+  const client = useApolloClient();
 
   useEffect(() => {
-    suggest(input).then((suggestions) => setSuggestions(suggestions));
-  }, [input]);
+    const ctx = { client };
+    suggest(ctx, input).then((suggestions) => setSuggestions(suggestions));
+  }, [input, client]);
 
   return suggestions;
 };
--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,3 +1,4 @@
+import { ApolloClient, gql } from "@apollo/client";
 import { decodeAddress, encodeAddress } from "@polkadot/util-crypto";
 import { SS58_PREFIX } from "../utils";
 
@@ -15,8 +16,16 @@ export type Suggestion =
   | BlockSuggestion
   | ExtrinsicSuggestion;
 
-export const suggest = async (input: string): Promise<Suggestion[]> => {
+export type SearchContext = {
+  client: ApolloClient<any>;
+};
+
+export const suggest = async (
+  ctx: SearchContext,
+  input: string
+): Promise<Suggestion[]> => {
   const promises = Promise.allSettled([
+    suggestHashLookup(input, ctx.client),
     suggestExtrinsic(input),
     suggestBlock(input),
     suggestAddress(input),
@@ -65,6 +74,17 @@ export const parseExtrinsicId = (input: string): [number, number] | null => {
   }
 };
 
+export const parseHexFixLength = (
+  input: string,
+  bytes: number
+): `0x${string}` | null => {
+  const re = new RegExp(`^0x[0-9a-zA-Z]{${bytes * 2}}$`, "g");
+  if (!input.match(re)) {
+    return null;
+  }
+  return input as `0x${string}`;
+};
+
 export const suggestAddress = async (
   input: string
 ): Promise<AddressSuggestion[]> => {
@@ -94,6 +114,71 @@ export const suggestExtrinsic = async (
   }
   const [blockNumber, extrinsicIndex] = extrinsicId;
   return [{ type: "extrinsic", value: `${blockNumber}-${extrinsicIndex}` }];
+};
+
+export const suggestHashLookup = async (
+  input: string,
+  client: ApolloClient<any>
+): Promise<Suggestion[]> => {
+  const hash = parseHexFixLength(input, 32);
+  if (!hash) {
+    return [];
+  }
+
+  const query = gql`
+    query SearchHash($hash: String!) {
+      extrinsics(filter: { hash: { equalTo: $hash } }) {
+        nodes {
+          id
+        }
+      }
+      blocks(filter: { hash: { equalTo: $hash } }) {
+        nodes {
+          id
+        }
+      }
+    }
+  `;
+  const variables = {
+    hash,
+  };
+
+  try {
+    const result = await client.query({ query, variables });
+
+    if (result.errors) {
+      console.warn(`Errors while searching hashes`, result.errors);
+    }
+
+    const { extrinsics, blocks } = result.data;
+
+    let suggestions: Suggestion[] = [];
+
+    if (extrinsics) {
+      suggestions = [
+        ...suggestions,
+        ...extrinsics.nodes.map((extrinsic: { id: string }) => ({
+          type: "extrinsic",
+          value: extrinsic.id,
+        })),
+      ];
+    }
+
+    if (blocks) {
+      suggestions = [
+        ...suggestions,
+        ...blocks.nodes.map((block: { id: string }) => ({
+          type: "block",
+          value: block.id,
+        })),
+      ];
+    }
+
+    return suggestions;
+  } catch (e: unknown) {
+    console.error(`Searching hash error: ${e}`);
+    return [];
+  }
 };
 
 export const suggestionRoute = (suggestion: Suggestion): string => {
