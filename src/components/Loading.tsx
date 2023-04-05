1e3af66f22c853c384b2711cb4327b0bcff1a06c
MOZGIII <mike-n@narod.ru>
2023-04-05 17:16:49 +0400

    Add a gap to the loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { Alert, Box, Spinner } from "@chakra-ui/react";
 export default function Loading() {
   return (
     <Box>
-      <Alert status="info">
+      <Alert status="info" gap="4px">
         <Spinner />
         Loading
       </Alert>
