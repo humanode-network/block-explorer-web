462d4e4ea3bd40b326df508e1b9615987f7c9250
MOZGIII <mike-n@narod.ru>
2023-04-06 02:59:32 +0400

    wtf is this bs, who does thi=emeing like this, chakra why

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,23 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   config: {
     initialColorMode: "dark",
   },
+  colors: {
+    brand: {
+      100: "rgb(253, 131, 69)",
+      900: "rgb(224, 52, 48)",
+    },
+    // brand: baseTheme.colors.red,
+  },
   styles: {
     global: {
       body: {
         bg: "rgb(25 25 25)",
+      },
+      a: {
+        color: "brand.100",
       },
     },
   },
